27736309f685dfa70a30f4f73ab0d543f0c251cf
Alexander Parkhomenko <42373411+senior-cherry@users.noreply.github.com>
2025-12-18 11:24:25 +0200

    init: setup project with eslint/prettier

---
+++ weather-spa/eslint.config.mjs
@@ -0,0 +1,34 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettier,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      'no-unused-vars': 'off',
+
+      'max-len': [
+        'warn',
+        {
+          code: 100,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+        },
+      ],
+    },
+  },
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+]);
